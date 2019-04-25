f92ade5aa2d6dd9c7aee64f78288b576e8a56778
Robert Gougelet <rgougelet@gmail.com>
2019-04-25 12:42:00 -0700

    commented eeg_3, made eeg+behav regression more flexible

--- darts_eeg_3_get_theta_and_alpha_power.m
+++ darts_eeg_3_get_theta_and_alpha_power.m
@@ -7,45 +7,47 @@ addpath(data_dir);
 eeglab;
 close all;
 
+% user input
+chan_labs = {'Fz','Cz','Pz','Oz'};
+freq_labs = {'theta', 'alpha'};
+subjs_to_include = {'571', '579', '580', ...
+	'607', '608', '616', '619', '621', '627', '631'};
+subjs_to_include = { '571', '608', '607', '579', '580'};
+
 % load XLSX SNAP data
 [num,txt,raw] = xlsread([data_dir,'behavioral_data.xlsx']);
 headers = txt(1,:);
 for k=1:numel(headers)
 	xlsx.(headers{k})=num(:,k) ;
 end
-chan_labs = {'Fz','Cz','Pz','Oz'};
-freq_labs = {'theta', 'alpha'};
+
+% initialize eeg columns to add the behav columns
 for chan_lab = chan_labs
 	for freq_lab = freq_labs
 		xlsx.([chan_lab{:},'_',freq_lab{:}]) = nan(length(xlsx.pres),1);
 	end
 end
 
-subjs_to_include = {'571', '579', '580', ...
-	'607', '608', '616', '619', '621', '627', '631'};
-subjs_to_include = { '571', '608', '607', '579', '580'};
-
 for subj_i = 1:length(subjs_to_include)
-
 	subj_id = subjs_to_include{subj_i};
 	subj_set = [subj_id,'_eeg_64_laplace.set'];
 	
+	% bandpass filter at desired freqs
 	EEG = pop_loadset('filename',subj_set,'filepath',data_dir);
-
 	EEG_theta = pop_eegfiltnew(EEG, 3, 7);
 	EEG_alpha = pop_eegfiltnew(EEG, 8, 12);
 	EEGs = {EEG_theta,EEG_alpha};
 	load([data_dir, subj_id,'_eeg_64_latencys'])
 	
-	% check trial alignment, astronomically small chance this check can fail
-	delays = [];
+	% check eeg:behav trial alignment, astronomically small chance this check can fail
 	for xl_start_row  = 1:(length(xlsx.pres)-length(start_event_time_inds))
 		xl_pres = xlsx.pres(xl_start_row:xl_start_row+length(start_event_time_inds)-1);
 		if sum(end_event_strings(:,4)==num2str(xl_pres)) == length(start_event_time_inds)
-		 break
-	 end
+			break
+		end
 	end
 
+	% retrieve this subjs trial-level amplitudes at desired chans and freqs
 	for chan_lab_i = 1:length(chan_labs)
 		chan_lab = chan_labs{chan_lab_i};
 		for freq_lab_i = 1:length(EEGs)
@@ -64,21 +66,27 @@ for subj_i = 1:length(subjs_to_include)
 	end	
 end
 
-% trial level accuracy regression
+% trial-level behav+eeg regression
 throwtimes = xlsx.throwtime(xlsx.distance < 6 & xlsx.throwtime < 5);
 delay = xlsx.delaystilltime(xlsx.distance < 6 & xlsx.throwtime < 5);
-pres = xlsx.pres(xlsx.distance < 6 & xlsx.throwtime < 5);
+pres = categorical(xlsx.pres(xlsx.distance < 6 & xlsx.throwtime < 5));
 dists = xlsx.distance(xlsx.distance < 6 & xlsx.throwtime < 5);
 subjs = categorical(xlsx.subject(xlsx.distance < 6 & xlsx.throwtime < 5));
-Fz_thetas = xlsx.Fz_theta(xlsx.distance < 6 & xlsx.throwtime < 5);
-reg_table = table(throwtimes,delay,dists,Fz_thetas);
-reg_table.pres = categorical(pres);
-reg_table.subjs = categorical(subjs);
 
-fit = fitlm(reg_table,'dists~throwtimes+delay+pres+subjs+Fz_thetas', 'RobustOpts', 'on');
+% create chan+freq  variables
+varnames = {'dists', 'throwtimes', 'delay', 'pres', 'subjs'};
+for chan_lab = chan_labs
+	for freq_lab = freq_labs
+		varname = [chan_lab{:},'_',freq_lab{:}];
+		varnames{end+1} = varname;
+		eval([varname,' = xlsx.',varname,'(xlsx.distance < 6 & xlsx.throwtime < 5);']);
+	end
+end
 
-Cz_thetas_abs = xlsx.Cz_theta(xlsx.distance < 6 & xlsx.throwtime < 5 & xlsx.pres==0);
-Cz_thetas_pres = xlsx.Cz_theta(xlsx.distance < 6 & xlsx.throwtime < 5 & xlsx.pres==1);
+eval( ['reg_table = table(', strjoin(varnames,','),');'] );
 
-[H,P,CI,STATS] = ttest2(Cz_thetas_abs,Cz_thetas_pres)
-[H,P,CI,STATS] = ttest2(Cz_thetas_abs,Cz_thetas_pres, 'vartype', 'unequal')
+fit = fitlm(reg_table,'ResponseVar', 'dists', 'RobustOpts', 'on');
+
+
+% [H,P,CI,STATS] = ttest2(Cz_thetas_abs,Cz_thetas_pres)
+% [H,P,CI,STATS] = ttest2(Cz_thetas_abs,Cz_thetas_pres, 'vartype', 'unequal')
