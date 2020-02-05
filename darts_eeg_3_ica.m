4f7590face89f050696afe9bd1811cf1fe50f5c5
Robert Gougelet <rgougelet@gmail.com>
2020-02-05 12:12:00 -0800

    ica

--- darts_eeg_3_ica.m
+++ darts_eeg_3_ica.m
@@ -1,16 +1,16 @@
 clear; close all; clc;
-% script_dir = '/data/mobi/Darts/Analysis/Analysis_Sept-2019/darts/';
-script_dir = 'G:/darts/';
+script_dir = '/data/mobi/Darts/Analysis/Analysis_Sept-2019/darts/';
+% script_dir = 'G:/darts/';
 cd(script_dir);
 data_dir = [script_dir,'data/'];
 addpath(data_dir)
-% rmpath([script_dir,'/eeglab/'])
-% addpath('/data/common/matlab/eeglab')
+rmpath([script_dir,'/eeglab/'])
+addpath('/data/common/matlab/eeglab')
 eeglab nogui;
 
 subjs_to_include = {
 	'571'
-	'579'
+	% 	'579'
 	'580'
 	'607'
 	'608'
@@ -21,16 +21,21 @@ subjs_to_include = {
 	'631'
 	};
 srate = 512;
-
+%%
 % binica is parfor compatible
-for subj_i = 1:length(subjs_to_include)
-
+parfor subj_i = 1:length(subjs_to_include)
+	
 	% load dataset
 	subj_id = subjs_to_include{subj_i};
 	subj_set = dir([data_dir,subj_id,'*_trim.set']);
 	EEG = pop_loadset('filename',subj_set.name,'filepath',data_dir);
 	old_setname = EEG.setname;
-
+	
+	% reject epochs
+	rej_ep_inds = find(EEG.reject.rejmanual);
+	EEG.etc.pipeline{end+1} =  ['Epochs removed: ', num2str(rej_ep_inds)];
+	EEG = pop_rejepoch( EEG, rej_ep_inds ,0);
+	
 	% reject channels
 	if strcmp(subj_id, '580')
 		rej_chans = {'A12'};
@@ -43,19 +48,14 @@ for subj_i = 1:length(subjs_to_include)
 	else
 		rej_chans = {};
 	end
-
+	
 	rej_chan_inds = [];
 	for rej_chan = rej_chans
 		labs = {EEG.chanlocs.labels};
 		rej_chan_inds(end+1) = find(strcmp(labs,rej_chan));
 	end
 	EEG = pop_interp(EEG, rej_chan_inds, 'spherical');
-	EEG.etc.pipeline{end+1} =  ['Channels removed and interpolated: ', strjoin(rej_chans,', ')];
-
-	% reject epochs
-	rej_ep_inds = find(EEG.reject.rejmanual);
-	EEG.etc.pipeline{end+1} =  ['Epochs removed: ', num2str(rej_ep_inds)];
-	EEG = pop_rejepoch( EEG, find(EEG.reject.rejmanual) ,0);
+	EEG.etc.pipeline{end+1} =  ['Channels  removed and interpolated: ', num2str(rej_chan_inds)];
 	
 	% avg ref
 	EEG.nbchan = EEG.nbchan+1;
@@ -64,13 +64,13 @@ for subj_i = 1:length(subjs_to_include)
 	EEG = pop_reref(EEG, []);
 	EEG = pop_select( EEG,'nochannel',{'initialReference'});
 	EEG.etc.pipeline{end+1} =  'Average reref.';
- 
+	
 	% amica
 	% out_dir = [data_dir,'amicaResults/' EEG.setname,'/'];
 	% mkdir(out_dir);
 	% runamica15(EEG, 'num_chans', EEG.nbchan,...
-			% 'numprocs',24,...
-			% 'do_reject', 1, 'numrej', 15, 'rejsig', 3, 'rejint', 1, 'outdir',out_dir);
+	% 'numprocs',24,...
+	% 'do_reject', 1, 'numrej', 15, 'rejsig', 3, 'rejint', 1, 'outdir',out_dir);
 	% EEG.etc.amica  = loadmodout15([data_dir,'amicaResults/' EEG.setname,'/']);
 	% EEG.etc.amica.S = EEG.etc.amica.S(1:EEG.etc.amica.num_pcs, :); % Weirdly, I saw size(S,1) be larger than rank. This process does not hurt anyway.
 	% EEG.icaweights = EEG.etc.amica.W;
@@ -79,9 +79,10 @@ for subj_i = 1:length(subjs_to_include)
 	% EEG.setname = [old_setname,'_amica'];
 	% EEG = pop_saveset(EEG, 'filename', EEG.setname,'filepath', data_dir);
 	
-	continue
 	% binica
-	EEG = pop_runica(EEG, 'icatype','binica','extended',1,'interupt', 'off');
+	cd(data_dir);
+	EEG = pop_runica(EEG, 'icatype','binica','extended',1,'interupt', 'off', 'filenum', str2num(subj_id));
+	cd(script_dir);
 	EEG.setname = [old_setname,'_ic'];
 	EEG.etc.pipeline{end+1} =  'BinICA run.';
 	EEG.etc.pipeline{end+1} =  EEG.icaweights;
