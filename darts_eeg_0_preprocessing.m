3dda3bd5ead4295005c35e5db825e9addb35f02f
Robert Gougelet <rgougelet@gmail.com>
2020-02-06 13:05:53 -0800

    redoing preproc, removing mains harmonics

--- darts_eeg_0_preprocessing.m
+++ darts_eeg_0_preprocessing.m
@@ -1,26 +1,34 @@
 clear; close all; clc;
-% script_dir = '/data/mobi/Darts/Analysis/Analysis_Sept-2019/darts/';
-script_dir = 'G:/darts/';
-
+script_dir = '/data/mobi/Darts/Analysis/Analysis_Sept-2019/darts/';
+% script_dir = 'G:/darts/';
 cd(script_dir);
 warning('off','MATLAB:rmpath:DirNotFound');
 rmpath('/data/common/matlab/eeglab')
 addpath([script_dir,'/eeglab/'])
-eeglab;
-close all;
+eeglab nogui;
 data_dir = [script_dir,'/data/'];
 addpath(data_dir)
 
-subjs_to_include = {'571', '579', '580', ...
-	'607', '608', '616', '619', '621', '627', '631'};
+subjs_to_include = {
+	'571'
+	'579'
+	'580'
+	'607'
+	'608'
+	'616'
+	'619'
+	'621'
+	'627'
+	'631'
+	};
 new_srate = 512;
 
 % preprocess sets
 % parfor compatible
 parfor subj_i = 1:length(subjs_to_include)
-
+	
 	% load dataset
-  subj_id = subjs_to_include{subj_i};
+	subj_id = subjs_to_include{subj_i};
 	subj_set = dir([subj_id,'_eeg.set']);
 	EEG = pop_loadset('filename',subj_set.name,'filepath',data_dir);
 	old_setname = EEG.setname;
@@ -49,7 +57,8 @@ parfor subj_i = 1:length(subjs_to_include)
 	EEG=pop_chanedit(EEG, 'changefield',{lheog_i,'labels','LHEOG'});
 	EEG=pop_chanedit(EEG, 'changefield',{rheog_i,'labels','RHEOG'});
 	EEG.etc.pipeline{end+1} =  'Externals relabeled';
-
+	EEG.etc.pipeline = EEG.etc.pipeline'; % make cell array easier to display
+	
 	% optimize head center
 	EEG = pop_chanedit(EEG, 'eval','chans = pop_chancenter( chans, [],[]);');
 	EEG.etc.pipeline{end+1} =  'Head center optimized';
@@ -57,26 +66,41 @@ parfor subj_i = 1:length(subjs_to_include)
 	% linked-mastoid reref
 	EEG = pop_reref(EEG, {'M1','M2'}, 'keepref','on');
 	EEG.etc.pipeline{end+1} =  'Linked-mastoid reref';
-
+	
 	% hp filter
 	EEG = pop_eegfiltnew(EEG, 1, 0, 1650, 0, [], 0);
 	EEG.etc.pipeline{end+1} =  ...
-			['eegfiltnew  HP, 1Hz, ',num2str(1650)];
+		['eegfiltnew  HP, 1Hz, ',num2str(1650)];
 	
 	% notch filter
 	EEG = pop_eegfiltnew(EEG, 59.5,60.5, [],true);
-		EEG.etc.pipeline{end+1} =  ...
-			['eegfiltnew  notch, 59.5 to 60.5 Hz'];
-		
+	EEG.etc.pipeline{end+1} =  ...
+		['eegfiltnew  notch, 59.5 to 60.5 Hz'];
+	
+	% notch filter
+	EEG = pop_eegfiltnew(EEG, 119.5,120.5, [],true);
+	EEG.etc.pipeline{end+1} =  ...
+		['eegfiltnew  notch, 119.5 to 120.5 Hz'];
+	
+	% notch filter
+	EEG = pop_eegfiltnew(EEG, 179.5,180.5, [],true);
+	EEG.etc.pipeline{end+1} =  ...
+		['eegfiltnew  notch, 179.5 to 180.5 Hz'];
+	
+	% notch filter
+	EEG = pop_eegfiltnew(EEG, 239.5,240.5, [],true);
+	EEG.etc.pipeline{end+1} =  ...
+		['eegfiltnew  notch, 239.5 to 240.5 Hz'];
+	
 	% resample
 	if EEG.srate ~= new_srate % keep old srate if equivalent
 		EEG = pop_resample( EEG, new_srate, 0.8, 0.4);
 		EEG.etc.pipeline{end+1} =  ...
-		['Resampled from ',num2str(EEG.srate),' to ',num2str(new_srate)];
+			['Resampled from ',num2str(EEG.srate),' to ',num2str(new_srate)];
 	end
 	
 	% save set
-	EEG.setname = [old_setname,'_',num2str(new_srate)];
+	EEG.setname = [old_setname,'_',num2str(EEG.srate)];
 	EEG.etc.pipeline{end+1} =  ['Saved as ',EEG.setname,' to ',data_dir,' at ', datestr(now)];
 	EEG = pop_saveset(EEG, 'filename', EEG.setname,'filepath', data_dir);
 end
