c59a942f178915dc2a99981345c5de1eb9ee0229
Robert Gougelet <rgougelet@gmail.com>
2020-02-12 16:13:43 -0800

    Update darts_eeg_5_iclabel.m

--- darts_eeg_5_iclabel.m
+++ darts_eeg_5_iclabel.m
@@ -6,7 +6,7 @@ warning('off','MATLAB:rmpath:DirNotFound');
 rmpath('/data/common/matlab/eeglab/')
 addpath([script_dir,'eeglab/'])
 addpath([script_dir,'deps/'])
-eeglab;
+eeglab nogui;
 data_dir = [script_dir,'data/'];
 addpath(data_dir)
 
@@ -22,13 +22,12 @@ subjs_to_include = {
 	'627'
 	'631'
 	};
-srate = 512;
 
 for subj_i = 1:length(subjs_to_include)
 	
 	% load dataset
 	subj_id = subjs_to_include{subj_i};
-	subj_set = dir([data_dir, subj_id,'*_ic.set']);
+	subj_set = dir([data_dir, subj_id,'*_ch.set']);
 	EEG = pop_loadset('filename',subj_set.name,'filepath',data_dir);
 	old_EEG = EEG;
 
@@ -41,13 +40,12 @@ for subj_i = 1:length(subjs_to_include)
 	% reduce to 4 channels
 	% 	chans2keep = {'C11','C26','D7','D17'}; % Fz, Cz, Pz, Oz
 	% 	EEG = pop_select(EEG,'channel',chans2keep);
+
 	EEG = pop_iclabel(EEG, 'default');
 	lab = EEG.etc.ic_classification.ICLabel;
 	classes = lab.classes;
 	[sort_c, sort_i] = sort(lab.classifications(:,1),'ascend');
-	eegplot(EEG.icaact(sort_i,:), 'dispchans',32, 'limits', [0 5], 'winlength',5)
-	figure; pwelch(EEG.icaact(:,:)',5000,20,[],512,'onesided');
-% 	eegplot(EEG.icaact(sort_i,:)./std(EEG.icaact(sort_i,:),[],2), 'dispchans',32, 'limits', [0 5], 'normed',1)
+
 	% save set
 	EEG.setname = [EEG.setname,'_lab'];
 	pop_saveset(EEG, 'filename', EEG.setname,'filepath', data_dir);
