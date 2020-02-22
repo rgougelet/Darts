4a928d5340d804b75be764320ff096bbcd8c64ea
Robert Gougelet <rgougelet@gmail.com>
2020-02-22 12:21:26 -0800

    redo regression, etc.

--- darts_eeg_8_run_regression_model.m
+++ darts_eeg_8_run_regression_model.m
@@ -18,7 +18,7 @@ subjs_to_include = {
 srate = 512;
 
 % make regression vars
-load('ic_r.mat');
+load('lap_ic_r.mat');
 r = struct2table(r);
 r(any(isnan(r.Variables),2),:) = [];
 
@@ -35,8 +35,6 @@ end
 pres = r.pres;
 r.pres = [];
 r.Variables = normalize(r.Variables);
-% rej_i = any(abs(r.Variables)>10,2);
-% sum(rej_i)
 rej_i = false(size(pres));
 r(rej_i,:) = [];
 r.trialnum = []; r.delay = []; r.delaystilltime = [];
@@ -60,27 +58,31 @@ bvarnames = {'pres'};
 
 % create time-averaged eeg columns
 for field = pvarnames
-	r.(field{:}) = r.(field{:})./r.throwtime;
+	r.(field{:}) = normalize(r.(field{:})./r.throwtime);
 end
 for field = dvarnames
-	r.(field{:}) = r.(field{:})./r.delaytime;
+	r.(field{:}) = normalize(r.(field{:})./r.delaytime);
 end
 r.throwtime = [];
 r.delaytime = [];
 lin_r = r;
-r = table2struct(r);
+int_r = table2struct(lin_r);
+int2_r = table2struct(lin_r);
+
 % create coupling terms
 vars = {pvarnames{:}}';
 vcs = nchoosek(vars,2);
 for vc_i = 1:length(vcs)
 	v1n = vcs{vc_i,1};
 	v2n = vcs{vc_i,2};
-	v1 = num2cell([r.pres].*[r.(v1n)]);
-	v2 = num2cell([r.pres].*[r.(v2n)]);
-	v12 = num2cell([r.pres].*[r.(v1n)].*[r.(v2n)]);
-	[r.(['presx',v1n])] = v1{:};
-	[r.(['presx',v2n])]= v2{:};
-	[r.(['presx',v1n,'x',v2n])] = v12{:};
+	v1 = num2cell(([r.pres].*[r.(v1n)]));
+	v2 = num2cell(([r.pres].*[r.(v2n)]));
+	v12 = num2cell(([r.pres].*[r.(v1n)].*[r.(v2n)]));
+	[int_r.(['presx',v1n])] = v1{:};
+	[int_r.(['presx',v2n])]= v2{:};
+	[int2_r.(['presx',v1n])] = v1{:};
+	[int2_r.(['presx',v2n])]= v2{:};
+ 	[int2_r.(['presx',v1n,'x',v2n])] = v12{:};
 end
 vars = {dvarnames{:}}';
 vcs = nchoosek(vars,2);
@@ -90,49 +92,43 @@ for vc_i = 1:length(vcs)
 	v1 = num2cell([r.pres].*[r.(v1n)]);
 	v2 = num2cell([r.pres].*[r.(v2n)]);
 	v12 = num2cell([r.pres].*[r.(v1n)].*[r.(v2n)]);
-	[r.(['presx',v1n])] = v1{:};
-	[r.(['presx',v2n])]= v2{:};
-	[r.(['presx',v1n,'x',v2n])] = v12{:};
+	[int_r.(['presx',v1n])] = v1{:};
+	[int_r.(['presx',v2n])]= v2{:};
+	[int2_r.(['presx',v1n])] = v1{:};
+	[int2_r.(['presx',v2n])]= v2{:};
+ 	[int2_r.(['presx',v1n,'x',v2n])] = v12{:};
 end
-r = struct2table(r);
-int_r = r;
+int_r = struct2table(int_r);
+int2_r = struct2table(int2_r);
 
-clc
 y = [r.distance]';
 ref = [sum((y-mean(y)).^2),...
 	sum((y-median(y)).^2),...
 	sum(abs(y-mean(y))),...
 	sum(abs(y-median(y)))...
 	]';
-ref_press = 0;
+ref_mean_press = 0;
 for i = 1:length(y)
 	pop_y = y;
 	pop_y(i) = [];
-	ref_press = ref_press + (y(i)-mean(pop_y)).^2;
+	ref_mean_press = ref_mean_press + (y(i)-mean(pop_y)).^2;
 end
-% mdl = fitlm(r,'distance~1');
-% r_median_absolute_deviation = nanmedian(abs(mdl.Residuals.Raw));
-% r_mean_absolute_deviation = nanmean(abs(mdl.Residuals.Raw));
-% r_root_mean_squared_error = mdl.RMSE;
-% r_root_median_squared_error = sqrt(nanmedian(mdl.Residuals.Raw.^2));
-% ref = [r_median_absolute_deviation;
-% 	r_mean_absolute_deviation;
-% 	r_root_mean_squared_error;
-% 	r_root_median_squared_error]
-% y = [r.distance]';
-%
-% mdl = fitlm(ones(size(y))*median(y),y,'Intercept',false,'RobustOpts','on');
-% r_median_absolute_deviation = nanmedian(abs(mdl.Residuals.Raw));
-% r_mean_absolute_deviation = nanmean(abs(mdl.Residuals.Raw));
-% r_root_mean_squared_error = mdl.RMSE;
-% r_root_median_squared_error = sqrt(nanmedian(mdl.Residuals.Raw.^2));
-% rb_ref = [r_median_absolute_deviation;
-% 	r_mean_absolute_deviation;
-% 	r_root_mean_squared_error;
-% 	r_root_median_squared_error]
-%% linear model
+ref_median_press = 0;
+for i = 1:length(y)
+	pop_y = y;
+	pop_y(i) = [];
+	ref_median_press = ref_median_press + (y(i)-median(pop_y)).^2;
+end
+%% regression
+close all;
+figure(1); figure(2);
+pause(1)
 clc
-r = table2struct(lin_r);
+int = false; % should be false with pres as a dummy variable
+% r = table2struct(lin_r); % linear model
+% r = table2struct(int_r); % linear+int model
+r = table2struct(int2_r); % linear+second order int model
+
 y = [r.distance]';
 X = r;
 fields = fieldnames(X);
@@ -140,122 +136,46 @@ X = rmfield(X,'distance');
 X = cell2mat(struct2cell(X))';
 
 % non-robust
-mdl = fitlm(X,y,'VarNames',fields, 'ResponseVar','distance');
-ref
+mdl = fitlm(X,y,'VarNames',fields, 'ResponseVar','distance', 'intercept', int);
 [sum((mdl.Residuals.Raw).^2)<ref,...
 	sum(abs(mdl.Residuals.Raw))<ref]'
-press([X,y])<ref_press
+ref_mean_press
+press_var = press([X,y])
 
-%% robust
-mdl = fitlm(X(ind,:),y(ind),'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance', 'intercept',false);
+% non-robust bootstrap
+mdl = fitlm(X,y,'VarNames',fields, 'ResponseVar','distance', 'intercept', int);
+bhs = runboot(X,y,mdl.Residuals.Raw,100000);
+quantile(bhs,[0.05, .95]);
+figure(1);
+ncoef = size(bhs,2);
+for i = 1:ncoef
+	subplot( round(sqrt(ncoef)) , ceil(sqrt(ncoef)) , i); hist(bhs(:,i))
+end
 
+% robust
+mdl = fitlm(X,y,'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance', 'intercept', int);
+[sum((mdl.Residuals.Raw).^2)<ref,...
+	sum(abs(mdl.Residuals.Raw))<ref]'
 press_var = 0;
 for i = 1:length(y)
-	if mod(i,100) == 0; disp([num2str(100*(i/length(y))),'% done']);	end
-	ind =true(size(y));
+	if mod(i,100) == 0
+		disp([num2str(100*(i/length(y))),'% done']);	
+	end
+	ind = true(size(y));
 	ind(i) = false;
-	mdl = fitlm(X(ind,:),y(ind),'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance', 'intercept',false);
+	mdl = fitlm(X(ind,:),y(ind),'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance', 'intercept',int);
 	rb =  mdl.Coefficients.Estimate;
-	press_var = press_var + (y(i) - X(i,:)*rb);
+	press_var = press_var + ((y(i) - X(i,:)*rb).^2);
 end
-press_var<ref_press
-[sum((mdl.Residuals.Raw).^2)<ref,...
-	sum(abs(mdl.Residuals.Raw))<ref]'
-%% linear+interaction model
-clc
-r = table2struct(int_r);
-y = [r.distance]';
-X = r;
-fields = fieldnames(X);
-X = rmfield(X,'distance');
-X = cell2mat(struct2cell(X))';
-b = X\y;
-res = X*b;
-mdl = fitlm(X,y,'VarNames',fields, 'ResponseVar','distance');
-[r_mad, r_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
-mdl = fitlm(X,y,'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance');
-[rb_mad, rb_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
+ref_mean_press
+press_var
 
-%% linear+interaction model
-clc
-r = table2struct(lin_r);
-y = [r.distance]';
-X = r;
-fields = fieldnames(X);
-X = rmfield(X,'distance');
-X = cell2mat(struct2cell(X))';
-b = X\y;
-res = X*b;
-mdl = fitlm(X,y,'VarNames',fields);
-[r_mad, r_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
-mdl = fitlm(X,y,'VarNames',fields, 'RobustOpts','on');
-[rb_mad, rb_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
-
-%% linear+interaction model
-clc
-r = lin_r;
-vcn = {bvarnames{:}, pvarnames{:}, dvarnames{:}};
-vars = {pvarnames{:}}';
-vcs = nchoosek(vars,2);
-for vc_i = 1:length(vcs)
-	v1n = vcs{vc_i,1};
-	v2n = vcs{vc_i,2};
-	vcn{end+1} = ['pres:',v1n];
-	vcn{end+1} = ['pres:',v2n];
-	vcn{end+1} = ['pres:',v1n,':',v2n];
+% robust bootstrap
+mdl = fitlm(X,y,'VarNames',fields, 'RobustOpts','on', 'ResponseVar','distance', 'intercept', int);
+bhs = runboot(X,y,mdl.Residuals.Raw,100000);
+quantile(bhs,[0.05, .95]);
+figure(2);
+ncoef = size(bhs,2);
+for i = 1:ncoef
+	subplot( round(sqrt(ncoef)) , ceil(sqrt(ncoef)) , i); hist(bhs(:,i))
 end
-
-vars = {dvarnames{:}}';
-vcs = nchoosek(vars,2);
-for vc_i = 1:length(vcs)
-	v1n = vcs{vc_i,1};
-	v2n = vcs{vc_i,2};
-	vcn{end+1} = ['pres:',v1n];
-	vcn{end+1} = ['pres:',v2n];
-	vcn{end+1} = ['pres:',v1n,':',v2n];
-end
-
-eqn = 'distance ~ ';
-eqn = [eqn, strjoin(vcn,' + ')];
-
-mdl = fitlm(r,eqn);
-[r_mad, r_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
-mdl = fitlm(r,eqn, 'RobustOpts','on');
-x = (mdl.Formula.Terms*table2array(mdl.Variables)')';
-[~,c,~] = collintest(x,'Display','off');
-[rb_mad, rb_rmse]
-[nanmedian(abs(mdl.Residuals.Raw)),...
-	mdl.RMSE,...
-	mdl.Rsquared.Adjusted,...
-	coefTest(mdl)...
-	]
-
-%% linear+interaction bootstrap
-clc
-x = (mdl.Formula.Terms*table2array(mdl.Variables)')';
-bhs = runboot(x,r.distance,mdl.Residuals.Raw,10000);
--- deps/runboot.m
+++ deps/runboot.m
@@ -2,9 +2,8 @@ function bhs = runboot(X,y,res,n_boot)
 	bhs = nan(n_boot,size(X,2));
 	nanIdx = any(isnan(X),2) | isnan(y) | isnan(res); 
 	res = res(~nanIdx); X = X(~nanIdx,:); y = y(~nanIdx);
-	X = normalize(X);
 	for i = 1:n_boot
-		if mod(i,1000) == 0
+		if mod(i,round(n_boot/20)) == 0
 			disp([num2str(100*(i/n_boot)),'% done']);
 		end
 		sh = randi(length(res),length(res),1);
